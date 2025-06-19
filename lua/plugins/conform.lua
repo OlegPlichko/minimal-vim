0712352c59d919583cbd18560c001776a00149ae
Oleg Plichko <oleg.plichko@sanofi.com>
2025-06-19 18:55:07 +0200

    format command

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,11 +6,15 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "goimports", "golines", "gofmt" },
+				typescriptreact = { "prettier" },
 			},
-			-- format_on_save = {
-			--     lsp_fallback = true,
-			--     async = false,
-			-- },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+			},
 		})
+		vim.api.nvim_create_user_command("Format", function()
+			require("conform").format({ async = true })
+		end, { desc = "Format the current buffer with conform" })
 	end,
 }
